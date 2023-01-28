c1a7929e2109eef177f1480175fff929fefb4ba5
Analise <analise.giobbi@gc.com>
2023-01-28 11:25:28 -0500

    bare bones of the notes router. needs test

---
+++ routes/notes.js
@@ -0,0 +1,32 @@
+const notes = require('express').Router()
+
+//do I need helper files?
+
+notes.get('/', (req, res) =>{
+    console.log(`${req.method} request recieved`)
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+})
+
+notes.post('/', (req,res) =>{
+    console.log(`${req.method} request recieved`)
+
+    const { text, title } = req.body
+
+    if (text && title){
+        const newNote = {
+            text,
+            title,
+        }
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response= {
+            status: "You have successfully added a note",
+            body: newNote,
+        }
+        res.json(response)
+    }else{
+        res.json('error in posting your new note')
+    }
+
+})
