a9c7b0c1e9ed7faec5b90c124279028843adbe56
Analise <analise.giobbi@gc.com>
2023-01-28 10:49:07 -0500

    started with the server file. No connections yet to routes

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path')
+const api = require('./routes/index.js')
+
+PORT = 3001;
+
+app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// First, need the get route for the home page
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// get the route for the notes html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+
+});
+
+// any wild card routes will take you to the home page
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () =>{
+    console.log(`your server is running under port ${PORT}`)
+})
+
